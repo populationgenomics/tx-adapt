c3431074b60eda17dbfae910acba16dc914dcaa9
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-07-13 17:28:57 +1000

    Copy chromosome 22 into test bucket

--- data_transfer/copy_gtex_v8_whole_blood_data.sh
+++ data_transfer/copy_gtex_v8_whole_blood_data.sh
@@ -3,3 +3,4 @@
 set -ex
 
 gsutil -u tx-adapt -m cp "gs://gtex-resources/GTEx_Analysis_v8_QTLs/GTEx_Analysis_v8_EUR_eQTL_all_associations/Whole_Blood.v8.EUR.allpairs.*" gs://cpg-gtex-main/v8/whole_blood/
+gsutil -u tx-adapt -m cp gs://gtex-resources/GTEx_Analysis_v8_QTLs/GTEx_Analysis_v8_EUR_eQTL_all_associations/Whole_Blood.v8.EUR.allpairs.chr22.parquet gs://cpg-gtex-test/v8/whole_blood/

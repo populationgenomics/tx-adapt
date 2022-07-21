1ac4ed7185a95ae9b550534661792b750d53aacc
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-07-21 14:59:23 +1000

    Remove click command --input-dir to simplify script and help debug

--- vep/run_vep.py
+++ vep/run_vep.py
@@ -5,20 +5,16 @@
 Run VEP on GTEx dataset
 """
 
-import click
 import hail as hl
 import pandas as pd
 from cpg_utils.hail_batch import (
     output_path,
 )
-from cloudpathlib import AnyPath
+
+GTEX = 'gs://cpg-gtex-test/v8/whole_blood/Whole_Blood.v8.EUR.allpairs.chr22.parquet'
 
 
-@click.command()
-@click.option(
-    '--input-path', required=True, help='Path to GTEx parquet files to run VEP on'
-)
-def main(input_path: str):
+def main():
     """
     Run vep using main.py wrapper
     """
@@ -26,10 +22,7 @@ def main(input_path: str):
     hl.init(default_reference='GRCh38')
 
     # read in all GTEX chromosome files
-    all_chromosome_files = list(
-        AnyPath(input_path).glob('*')  # pylint: disable=no-member
-    )
-    gtex = pd.concat((pd.read_parquet(f) for f in all_chromosome_files))
+    gtex = pd.read_parquet(GTEX)
     # add in necessary VEP annotation
     variant_id_info = gtex.variant_id.str.split('_').str[0:4]
     gtex['chr'], gtex['position'], gtex['alleles'] = (

84e3f0b8dc328a8b638087b7051cee6c5cfeaa6a
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-10-07 12:55:59 +1100

    Perform VEP annotation by intersection of ht, rather than running

--- vep/run_vep.py
+++ vep/run_vep.py
@@ -10,17 +10,19 @@ import hail as hl
 from hail.utils.java import Env
 from cpg_utils.hail_batch import output_path
 
-# from cloudpathlib import AnyPath
-GTEX_FILE = (
-    'gs://cpg-gtex-test/v8/whole_blood/Whole_Blood.v8.EUR.allpairs.chr22.parquet'
+# VEP 95 (GENCODE 29), which is closest to GENCODE 26
+VEP_HT = (
+    'gs://gcp-public-data--gnomad/resources/context/grch38_context_vep_annotated.gtex/'
 )
 CADD_HT = 'gs://cpg-reference/seqr/v0-1/combined_reference_data_grch38-2.0.4.ht'
 GENCODE_GTF = 'gs://cpg-gtex-test/reference/gencode.v26.annotation.gtf.gz'
 
 
 @click.command()
-@click.option('--vep-version', help='Version of VEP', default='104.3')
-def main(vep_version: str):
+@click.option('--gtex-file', help='gtex file to perform VEP annotation on')
+@click.option('--tissue-type', help='tissue type of gtex file')
+@click.option('--chromosome', help='chromosome of gtex file (without chr prefix)')
+def main(gtex_file: str, tissue_type: str, chromosome: str):
     """
     Run vep using main.py wrapper
     """
@@ -29,48 +31,43 @@ def main(vep_version: str):
 
     spark = Env.spark_session()
 
-    gtex = spark.read.parquet(GTEX_FILE)
-    ht = hl.Table.from_spark(gtex)
+    gtex = spark.read.parquet(gtex_file)
+    gtex = hl.Table.from_spark(gtex)
 
-    # add in necessary VEP annotation
-    ht = ht.annotate(
-        chromosome=ht.variant_id.split('_')[0],
-        position=ht.variant_id.split('_')[1],
-        alleles=ht.variant_id.split('_')[2:4],
+    # prepare ht for VEP annotation
+    gtex = gtex.annotate(
+        chromosome=gtex.variant_id.split('_')[0],
+        position=gtex.variant_id.split('_')[1],
+        alleles=gtex.variant_id.split('_')[2:4],
     )
-    ht = ht.annotate(locus=hl.locus(ht.chromosome, hl.int32(ht.position)))
+    gtex = gtex.annotate(locus=hl.locus(gtex.chromosome, hl.int32(gtex.position)))
     # 'vep' requires the key to be two fields: 'locus' (type 'locus<any>') and 'alleles' (type 'array<str>')
-    ht = ht.key_by('locus', 'alleles')
-    # filter to biallelic loci only
-    ht = ht.filter(hl.len(ht.alleles) == 2)
-    # remove starred alleles, as this results in an error in VEP
-    # see https://discuss.hail.is/t/vep-output-variant-not-found-in-original-variants/1148
-    ht = ht.filter(ht.alleles[1] != '*')
-    vep = hl.vep(ht, config='file:///vep_data/vep-gcloud.json')
-    # save vep ht
-    vep_path = output_path(f'vep{vep_version}.ht')
-    vep.write(vep_path, overwrite=True)
-    # only keep GTEx annotation and the most severe consequences from VEP annotation
-    gtex_entries = list(ht.row)
-    keys = list(ht.key)
-    gtex_entries = [name for name in gtex_entries if name not in keys]
-    vep = vep.select(
-        *gtex_entries,
-        vep.vep.most_severe_consequence,
-        vep.vep.transcript_consequences,
+    gtex = gtex.key_by('locus', 'alleles')
+
+    # add in VEP annotation and match with gtex association data
+    vep = hl.read_table(VEP_HT)
+    vep = vep[gtex.key].vep
+    # only keep VEP annotation that's relevant for TA analysis
+    gtex = gtex.annotate(
+        most_severe_consequence=vep.most_severe_consequence,
+        consequence_terms=vep.transcript_consequences.consequence_terms,
+        transcript_id=vep.transcript_consequences.transcript_id,
     )
+
     # add CADD annotation
-    cadd_ht = hl.read_table(CADD_HT)
-    vep = vep.annotate(
-        cadd=cadd_ht[vep.key].cadd,
+    cadd = hl.read_table(CADD_HT)
+    gtex = vep.annotate(
+        cadd=cadd[vep.key].cadd,
     )
     # add in ensembl ids
     gtf = hl.experimental.import_gtf(
         GENCODE_GTF, reference_genome='GRCh38', skip_invalid_contigs=True, force=True
     )
-    vep = vep.annotate(gene_id=gtf[vep.locus].gene_id)
-    vep_path = output_path(f'vep{vep_version}_cadd_GRCh38_annotation.tsv.bgz')
-    vep.write(vep_path, overwrite=True)
+    gtex = gtex.annotate(gene_id=gtf[gtex.locus].gene_id)
+    gtex_path = output_path(
+        f'gtex_association_{tissue_type}_chr{chromosome}_vep95_cadd_annotated.tsv.bgz'
+    )
+    gtex.write(gtex_path, overwrite=True)
 
 
 if __name__ == '__main__':

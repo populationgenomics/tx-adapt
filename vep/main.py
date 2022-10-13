406b82ee6c533a4756ec371dff33ec1d9bb22858
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-10-13 11:12:28 +1100

    Also increase the run time

--- vep/main.py
+++ vep/main.py
@@ -14,7 +14,7 @@ batch = get_batch('run_vep_in_dataproc_cluster')
 dataproc.hail_dataproc_job(
     batch,
     f'run_vep.py',
-    max_age='1h',
+    max_age='4h',
     num_secondary_workers=20,
     init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
     job_name='run_vep',

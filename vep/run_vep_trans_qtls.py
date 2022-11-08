a31b047928116692b21bf782181d9a36f405a42d
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-11-08 21:13:15 +1100

    Print out hail table after loading, to debug

--- vep/run_vep_trans_qtls.py
+++ vep/run_vep_trans_qtls.py
@@ -36,38 +36,39 @@ def main():
         },
     )
 
-    # prepare ht for VEP annotation
-    gtex = gtex.annotate(
-        chromosome=gtex.variant_id.split('_')[0],
-        position=gtex.variant_id.split('_')[1],
-        alleles=gtex.variant_id.split('_')[2:4],
-    )
-    gtex = gtex.annotate(locus=hl.locus(gtex.chromosome, hl.int32(gtex.position)))
-    # 'vep' requires the key to be two fields: 'locus' (type 'locus<any>') and 'alleles' (type 'array<str>')
-    gtex = gtex.key_by('locus', 'alleles')
-    # add in VEP annotation and match with gtex association SNV data
-    vep = hl.read_table(VEP_HT)
-    vep = vep[gtex.key].vep
-    # only keep VEP annotation that's relevant for TA analysis
-    gtex = gtex.annotate(
-        most_severe_consequence=vep.most_severe_consequence,
-        consequence_terms=vep.transcript_consequences.consequence_terms,
-        transcript_id=vep.transcript_consequences.transcript_id,
-    )
+    print(gtex.show())
+    # # prepare ht for VEP annotation
+    # gtex = gtex.annotate(
+    #     chromosome=gtex.variant_id.split('_')[0],
+    #     position=gtex.variant_id.split('_')[1],
+    #     alleles=gtex.variant_id.split('_')[2:4],
+    # )
+    # gtex = gtex.annotate(locus=hl.locus(gtex.chromosome, hl.int32(gtex.position)))
+    # # 'vep' requires the key to be two fields: 'locus' (type 'locus<any>') and 'alleles' (type 'array<str>')
+    # gtex = gtex.key_by('locus', 'alleles')
+    # # add in VEP annotation and match with gtex association SNV data
+    # vep = hl.read_table(VEP_HT)
+    # vep = vep[gtex.key].vep
+    # # only keep VEP annotation that's relevant for TA analysis
+    # gtex = gtex.annotate(
+    #     most_severe_consequence=vep.most_severe_consequence,
+    #     consequence_terms=vep.transcript_consequences.consequence_terms,
+    #     transcript_id=vep.transcript_consequences.transcript_id,
+    # )
 
-    # add CADD annotation
-    cadd = hl.read_table(CADD_HT)
-    gtex = gtex.annotate(
-        cadd=cadd[gtex.key].cadd,
-    )
-    # add in ensembl ids
-    gtf = hl.experimental.import_gtf(
-        GENCODE_GTF, reference_genome='GRCh38', skip_invalid_contigs=True, force=True
-    )
-    gtex = gtex.annotate(variant_gene_id=gtf[gtex.locus].gene_id)
-    # export as ht
-    gtex_path_ht = output_path(f'trans_qtl_vep_gnomADcontext95_cadd_annotated.ht')
-    gtex.write(gtex_path_ht, overwrite=True)
+    # # add CADD annotation
+    # cadd = hl.read_table(CADD_HT)
+    # gtex = gtex.annotate(
+    #     cadd=cadd[gtex.key].cadd,
+    # )
+    # # add in ensembl ids
+    # gtf = hl.experimental.import_gtf(
+    #     GENCODE_GTF, reference_genome='GRCh38', skip_invalid_contigs=True, force=True
+    # )
+    # gtex = gtex.annotate(variant_gene_id=gtf[gtex.locus].gene_id)
+    # # export as ht
+    # gtex_path_ht = output_path(f'trans_qtl_vep_gnomADcontext95_cadd_annotated.ht')
+    # gtex.write(gtex_path_ht, overwrite=True)
 
 
 if __name__ == '__main__':
